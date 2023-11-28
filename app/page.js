dbf2222660b64cfac3586c9868c93ab71530a8c2
aguiarHugo <aguiarhugofx@gmail.com>
2023-11-28 11:56:44 -0300

    new section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import Product from './sections/Product'
+
 
 export default function Home() {
   return (
-   <>
+   <main className='bg-gsw_dark'>
     <Navbar />
     <Hero />
-   </>
+    <Product />
+   </main>
   )
 }
