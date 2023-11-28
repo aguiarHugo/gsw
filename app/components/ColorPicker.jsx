d2f1d6fe37ea70f6998aa1be5899d1994b0d587a
aguiarHugo <aguiarhugofx@gmail.com>
2023-11-28 13:54:58 -0300

    fixed imgUrl

--- app/components/ColorPicker.jsx
+++ app/components/ColorPicker.jsx
@@ -1,68 +1,71 @@
 'use client'
 
+import Image from "next/image";
 import React, { useState } from "react";
 
 const carModels = [
   {
     model: "Cinza Diamante",
     circleColor: "#656667",
-    imgUrl: "../assets/cinza-diamante.png",
+    imgUrl: "/cinza-diamante.png",
   },
   {
     model: "Azul Topázio",
     circleColor: "#255EC1",
-    imgUrl: "../assets/azul-topazio.png",
+    imgUrl: "/azul-topazio.png",
   },
   {
     model: "Branco Ágata",
     circleColor: "#D6D8D3",
-    imgUrl: "../assets/branco-agata.png",
+    imgUrl: "/branco-agata.png",
   },
   {
     model: "Marrom Citrino",
     circleColor: "#3C150E",
-    imgUrl: "../assets/marrom-citrino.png",
+    imgUrl: "/marrom-citrino.png",
   },
   {
     model: "Preto Hematita",
     circleColor: "#040404",
-    imgUrl: "../assets/preto-hematita.png",
+    imgUrl: "/preto-hematita.png",
   },
   {
     model: "Vermelho Granada",
     circleColor: "#4E0F17",
-    imgUrl: "../assets/preto-vermelho-granda.png",
+    imgUrl: "/vermelho-granada.png",
   },
 ];
 
 const ColorPicker = () => {
-  const [selectedModel, setSelectedModel] = useState(null);
-  const [selectedImgUrl, setSelectedImgUrl] = useState(null);
+  const [selectedModel, setSelectedModel] = useState(carModels[0].model);
+  const [selectedImgUrl, setSelectedImgUrl] = useState(carModels[0].imgUrl);
 
   const handleColorClick = (model, imgUrl) => {
     setSelectedModel(model);
     setSelectedImgUrl(imgUrl);
   };
 
-
   return (
-    <div className="mt-6 w-[400px] h-[45px] rounded-[51px] border-2 border-solid border-[#DAF40F] flex justify-around items-center mx-auto">
-      <div className="flex items-center gap-[20px]">
+    <div className="mt-6">
+      <div className="gap-[20px] w-[400px] h-[45px] rounded-[51px] border border-solid border-[#DAF40F] flex justify-around items-center mx-auto px-6">
         {carModels.map((item, itemIndex) => (
           <div
             key={itemIndex}
-            className={`${item.model === selectedModel && 'border-2 border-solid border-[#DAF40F]'} cursor-pointer h-[17px] w-[17px] rounded-full`}
-            onClick={() => handleColorClick(item.model)}
+            className={`${item.model === selectedModel && 'border border-solid border-[#DAF40F]'} cursor-pointer h-[17px] w-[17px] rounded-full relative`}
+            onClick={() => handleColorClick(item.model, item.imgUrl)}
             style={{ backgroundColor: item.circleColor }}
           ></div>
         ))}
+        <p className="text-[10px] min-w-[110px] text-white uppercase">{selectedModel}</p>
       </div>
-      <p className="text-[10px] min-w-[110px] text-white uppercase">{selectedModel}</p>
-      <img
+      {selectedImgUrl && (
+        <Image
           src={selectedImgUrl}
-          alt={`Imagem do modelo ${selectedModel}`}
-          className="h-auto w-auto"
-      />
+          width={481}
+          height={250}
+          className="mt-[-50px]"
+        />
+      )}
     </div>
   );
 };
