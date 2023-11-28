ba0e2932798d10c544c29541533765c330df5ae5
aguiarHugo <aguiarhugofx@gmail.com>
2023-11-28 11:57:23 -0300

    Styled

--- app/sections/Navbar.jsx
+++ app/sections/Navbar.jsx
@@ -4,9 +4,9 @@ import Image from "next/image"
 
 const Navbar = () => {
   return (
-    <nav className="w-full fixed z-10  ">
+    <nav className="w-full fixed z-10">
       {/* MOBILE */}
-      <div className=" flex items-center justify-between w-full py-4 px-8 bg-gsw_gray backdrop-blur-md bg-opacity-50 border-b border-gsw_green sm:hidden ">
+      <div className="flex items-center justify-between w-full py-4 px-8 bg-gsw_gray backdrop-blur-md bg-opacity-50 border-b border-gsw_green sm:hidden ">
         <div>
           <Image src='/LOGO-MOBILE.svg' width={78} height={26} alt="GWM" />
         </div>
@@ -16,7 +16,7 @@ const Navbar = () => {
       </div>
 
       {/* TABLET */}
-      <div className="sm:flex md:hidden hidden items-center justify-between w-full py-4 px-8 bg-gsw_gray backdrop-blur-md bg-opacity-50 border-b border-gsw_green font-bold text-gsw_white">
+      <div className="sm:flex lg:hidden hidden items-center justify-between w-full py-4 px-8 bg-gsw_gray backdrop-blur-md bg-opacity-50 border-b border-gsw_green font-semi-bold text-gsw_white">
         <a href="#types">MODELOS</a>
         <a href="#experience">GWM EXPERIENCE</a>
         <a href="#stores">CONCESSIONÁRIAS</a>
@@ -24,6 +24,15 @@ const Navbar = () => {
       </div>
 
       {/* DESKTOP */}
+      <div className="w-full block mt-[28px]">
+        <div className="hidden lg:flex lg:justify-center items-center w-[1360px] bg-gsw_gray py-2 border border-solid border-gsw_green backdrop-blur-md bg-opacity-50 rounded-[48px] mx-auto gap-10 font-semi-bold text-gsw_white">
+          <a href="#types">MODELOS</a>
+          <a href="#experience">GWM EXPERIENCE</a>
+          <Image src='/LOGO-MOBILE.svg' width={78} height={26} alt="GWM" />
+          <a href="#stores">CONCESSIONÁRIAS</a>
+          <a href="#contact">CONTATO</a>
+        </div>
+      </div>
     </nav>
   )
 }
